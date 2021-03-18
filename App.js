d516588b4ed8d022de3c7004caa085d4a9793147
David Yarzebinski <davidyarzebinski@cloudfitsoftware.com>
2021-03-18 16:09:21 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+import {addTodo, getTodosFromRealm} from './src/realm/todos.realm';
+
+const App = () => {
+  const todoItems = getTodosFromRealm();
+
+  // Add a new item to the state
+  function addTodoItem(_text) {
+    addTodo({text: _text, complete: false});
+  }
+
+  // Delete an item from state by index
+  function deleteTodoItem(_index) {
+    let tempArr = [...todoItems];
+    tempArr.splice(_index, 1);
+  }
+
+  // Function to set completed to true by index.
+  function completeTodoItem(_index) {
+    let tempArr = [...todoItems];
+    tempArr[_index].completed = true;
+  }
+
+  // Render
+  return (
+    <>
+      <StatusBar barStyle={'light-content'} backgroundColor={'#212121'} />
+      <SafeAreaView
+        style={{padding: 16, justifyContent: 'space-between', flex: 1}}>
+        <Text style={{fontSize: 36, fontWeight: 'bold'}}>Todo</Text>
+        <FlatList
+          data={todoItems}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, index}) => {
+            return (
+              <TodoItem
+                item={item}
+                deleteFunction={() => deleteTodoItem(index)}
+                completeFunction={() => completeTodoItem(index)}
+              />
+            );
+          }}
+        />
+        <TodoInput onPress={addTodoItem} />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ src/realm/todos.realm.js
@@ -0,0 +1,44 @@
+import Realm from 'realm';
+
+export const TodoSchema = {
+  name: 'Todo',
+  properties: {
+    id: 'int',
+    text: 'string',
+    complete: 'bool',
+  },
+  primaryKey: 'id',
+};
+
+const realmInstance = new Realm({
+  path: 'todos',
+  schema: [TodoSchema],
+  schemaVersion: 4,
+  migration: (oldRealm, newRealm) => {
+    if (oldRealm.schemaVersion !== newRealm.schemaVersion) {
+      newRealm.deleteAll();
+    }
+  },
+});
+
+export const getTodosFromRealm = () => {
+  return realmInstance.objects('Todo');
+};
+
+export const addTodo = todo => {
+  try {
+    const latestTodo = realmInstance.objects('Todo').sorted('id', true)[0];
+    const highestId = latestTodo == null ? 0 : latestTodo.id;
+    todo.id = highestId + 1;
+    realmInstance.beginTransaction();
+    realmInstance.create('Todo', {
+      id: todo.id,
+      text: todo.text,
+      complete: todo.complete,
+    });
+    realmInstance.commitTransaction();
+  } catch (err) {
+    console.error(err);
+    realmInstance.cancelTransaction();
+  }
+};
